27333f4df9a2e960373986eb3f872c2842336bf7
S. M. Fahim Rafi Siddiqui <s.m.fahim.ju@gmail.com>
2019-01-27 03:51:59 +0600

    m

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
 <?php
 
-function DrawCircle($count)
-{
-    echo " executing level $count <br/>";
 
-    if($count<5)
-        DrawCircle($count+1);
-
-    echo " Ending level $count <br/>";    
-}
-
-DrawCircle(1);
+phpinfo();
